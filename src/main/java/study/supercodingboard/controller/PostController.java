e8cc107fc7d66304a87a168327a35abe8671d99e
jeongwooa <ljw6027@naver.com>
2024-10-21 17:05:34 +0900

    y ommitted:

--- src/main/java/study/supercodingboard/controller/PostController.java
+++ src/main/java/study/supercodingboard/controller/PostController.java
@@ -1,10 +1,12 @@
 package study.supercodingboard.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
+@Controller
 public class PostController
 {
 
