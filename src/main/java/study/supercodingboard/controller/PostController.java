d49777311b242f777a69987b29fb0a6088ad08c0
jeongwooa <ljw6027@naver.com>
2024-10-21 15:44:38 +0900

    update PostController to fix bug

--- src/main/java/study/supercodingboard/controller/PostController.java
+++ src/main/java/study/supercodingboard/controller/PostController.java
@@ -1,4 +1,9 @@
 package study.supercodingboard.controller;
 
-public class PostController {
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostController
+{
+    
 }
