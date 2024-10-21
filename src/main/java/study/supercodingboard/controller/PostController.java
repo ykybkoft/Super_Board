adf4c0c12385c615028b6da9ebba4525d26c7d11
ykybkoft <126169447+ykybkoft@users.noreply.github.com>
2024-10-21 16:53:02 +0900

    commit-test11

--- src/main/java/study/supercodingboard/controller/PostController.java
+++ src/main/java/study/supercodingboard/controller/PostController.java
@@ -1,9 +1,11 @@
 package study.supercodingboard.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class PostController
 {
-    
+
 }
