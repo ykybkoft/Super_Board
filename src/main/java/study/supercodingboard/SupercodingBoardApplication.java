8252869b0d6278dd275758e806ebef26d3c27a04
fhfld <seehb126@naver.com>
2024-10-21 14:03:30 +0900

    ✨feature: 프로젝트 초기 설정

---
+++ src/main/java/study/supercodingboard/SupercodingBoardApplication.java
@@ -0,0 +1,13 @@
+package study.supercodingboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupercodingBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupercodingBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/supercodingboard/SupercodingBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package study.supercodingboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupercodingBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
