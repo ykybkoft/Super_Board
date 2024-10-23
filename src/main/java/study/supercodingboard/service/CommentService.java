daf2e237e30499789196f64363f2ab9c38231d98
jeongwooa <ljw6027@naver.com>
2024-10-23 17:12:13 +0900

    JPA로 수정

--- src/main/java/study/supercodingboard/service/CommentService.java
+++ src/main/java/study/supercodingboard/service/CommentService.java
@@ -1,37 +1,40 @@
 package study.supercodingboard.service;
 
-import study.supercodingboard.model.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import study.supercodingboard.entity.Comment;
 import study.supercodingboard.repository.CommentRepository;
 
 import java.util.List;
 
+@Service
 public class CommentService {
     private final CommentRepository commentRepository;
 
-
-    public CommentService() {
-        this.commentRepository = new CommentRepository(); // 생성자에서 CommentRepository 초기화
+    @Autowired
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository; // 생성자에서 CommentRepository 초기화
     }
 
     public void createComment(Comment comment) {
         commentRepository.save(comment); // 댓글을 저장하는 메서드 호출
     }
 
-    public List<Comment> findAll()
-    {
+    public List<Comment> findAll() {
         return commentRepository.findAll(); // 데이터베이스에서 모든 댓글을 조회하여 반환
     }
 
-
-    // 댓글 수정
     public void updateComment(Long commentId, String content) {
-        commentRepository.updateContent(commentId, content); // 댓글 내용을 수정하는 메서드 호출
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new RuntimeException());
+        comment.setContent(content); // 댓글 내용 수정
+        commentRepository.save(comment); // 변경 사항 저장
     }
 
-    // 댓글 삭제
-    public void deleteComment(Long commentId)
-    {
-        commentRepository.deleteContent(commentId);
-
+    public void deleteComment(Long commentId) {
+        if (!commentRepository.existsById(commentId)) {
+            throw new RuntimeException(); // 존재하지 않는 경우 예외 처리
+        }
+        commentRepository.deleteById(commentId); // 댓글 삭제
     }
 }
